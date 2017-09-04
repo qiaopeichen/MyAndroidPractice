6ca0d6c93ec76e1dc2e0b9cb6a80364b7b66c688
qiaopeichen <908694753@qq.com>
2017-09-04 16:00:05 +0800

    安全卫士 缓存清理，一键缓存暂未实现

--- mobilesafe74/app/src/main/java/com/itheima/mobilesafe74/activity/CacheClearActivity.java
+++ mobilesafe74/app/src/main/java/com/itheima/mobilesafe74/activity/CacheClearActivity.java
@@ -1,23 +1,34 @@
 package com.itheima.mobilesafe74.activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.IPackageDataObserver;
 import android.content.pm.IPackageStatsObserver;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageStats;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.itheima.mobilesafe74.R;
+import com.itheima.mobilesafe74.utils.ToastUtil;
+import com.tbruyelle.rxpermissions.RxPermissions;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Random;
 
 public class CacheClearActivity extends AppCompatActivity {
 
@@ -30,6 +41,45 @@ public class CacheClearActivity extends AppCompatActivity {
     private TextView tv_name;
     private LinearLayout ll_add_text;
     private PackageManager mPm;
+    private int mIndex = 0;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_CACHE_APP:
+                    // 8.在线型布局中添加有缓存应用条目
+                    View view = View.inflate(getApplicationContext(), R.layout.linearlayout_cache_item, null);
+                    ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+                    TextView tv_item_name = (TextView) view.findViewById(R.id.tv_name);
+                    TextView tv_memory_info = (TextView) view.findViewById(R.id.tv_memory_info);
+                    ImageView iv_delete = (ImageView) view.findViewById(R.id.iv_delete);
+                    final CacheInfo cacheInfo = (CacheInfo) msg.obj;
+                    iv_icon.setBackgroundDrawable(cacheInfo.icon);
+                    tv_item_name.setText(cacheInfo.name);
+                    tv_memory_info.setText(android.text.format.Formatter.formatFileSize(getApplicationContext(), cacheInfo.cacheSize));
+                    ll_add_text.addView(view, 0);
+                    iv_delete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
+                            intent.setData(Uri.parse("package:" + cacheInfo.packagename));
+                            startActivity(intent);
+                        }
+                    });
+                    break;
+                case CHECK_CACHE_APP:
+                    tv_name.setText((String) msg.obj);
+                    break;
+                case CHECK_FINISH:
+                    tv_name.setText("扫描完成");
+                    break;
+                case CLEAR_CACHE:
+                    // 从线性布局中移除所有的条目
+                    ll_add_text.removeAllViews();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +87,11 @@ public class CacheClearActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cache_clear);
         initUI();
         initData();
+
     }
 
     private void initData() {
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 // 1.获取包管理者对象
@@ -54,13 +105,40 @@ public class CacheClearActivity extends AppCompatActivity {
                     // 包名作为获取缓存信息的条件
                     String packageName = packageInfo.packageName;
                     getPackageCache(packageName);
+                    try {
+                        Thread.sleep(100 + new Random().nextInt(50));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    mIndex++;
+                    pb_bar.setProgress(mIndex);
+                    // 每循环一次就将检测应用的名称发送给主线程显示
+                    Message msg = Message.obtain();
+                    msg.what = CHECK_CACHE_APP;
+                    String name = null;
+                    try {
+                        name = mPm.getApplicationInfo(packageName, 0).loadLabel(mPm).toString();
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    msg.obj = name;
+                    mHandler.sendMessage(msg);
                 }
+                Message msg = Message.obtain();
+                msg.what = CHECK_FINISH;
+                mHandler.sendMessage(msg);
+
             }
         }.start();
     }
 
+    /**
+     * 通过包名获取此包名指向应用的缓存信息
+     *
+     * @param packageName 应用包名
+     */
     private void getPackageCache(String packageName) {
-        new IPackageStatsObserver.Stub() {
+        IPackageStatsObserver.Stub mStatsObserver = new IPackageStatsObserver.Stub() {
             @Override
             public void onGetStatsCompleted(PackageStats stats, boolean succeeded) throws RemoteException {
                 // 子线程中方法，用到消息机制
@@ -70,13 +148,40 @@ public class CacheClearActivity extends AppCompatActivity {
                 if (cacheSize > 0) {
                     // 6.告知主线程更新UI
                     Message msg = Message.obtain();
-
                     msg.what = UPDATE_CACHE_APP;
-                    //TODO
-
+                    CacheInfo cacheInfo = null;
+                    try {
+                        // 7.维护有缓存应用的javabean
+                        cacheInfo = new CacheInfo();
+                        cacheInfo.cacheSize = cacheSize;
+                        cacheInfo.packagename = stats.packageName;
+                        cacheInfo.name = mPm.getApplicationInfo(stats.packageName, 0).loadLabel(mPm).toString();
+                        cacheInfo.icon = mPm.getApplicationInfo(stats.packageName, 0).loadIcon(mPm);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    msg.obj = cacheInfo;
+                    mHandler.sendMessage(msg);
                 }
             }
+        };
+        // 1.获取指定类的字节码文件
+        try {
+            Class<?> clazz = Class.forName("android.content.pm.PackageManager");
+            // 2.获取调用方法对象
+            Method method = clazz.getMethod("getPackageSizeInfo", String.class, IPackageStatsObserver.class);
+            // 3.获取对象调用方法
+            method.invoke(mPm, packageName, mStatsObserver);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    class CacheInfo {
+        public String name;
+        public Drawable icon;
+        public String packagename;
+        public long cacheSize;
     }
 
     private void initUI() {
@@ -91,9 +196,22 @@ public class CacheClearActivity extends AppCompatActivity {
                 try {
                     Class<?> clazz = Class.forName("android.content.pm.PackageManager");
                     // 2.获取调用方法对象
-                    Method method = clazz.getMethod("freeStorageAndNotify", long.class, IPackageStatsObserver.class);
+                    Method method = clazz.getMethod("freeStorageAndNotify", long.class, IPackageDataObserver.class);
                     // 3.获取对象调用方法
-                    method.invoke(mPm, packageName, mStatsObserer);
+                    method.invoke(mPm, Long.MAX_VALUE, new IPackageDataObserver.Stub() {
+                        @Override
+                        public void onRemoveCompleted(String packageName, boolean succeeded) throws RemoteException {
+                            // 清除缓存完成后调用的方法（考虑权限）
+                            Message msg = Message.obtain();
+                            msg.what = CLEAR_CACHE;
+                            mHandler.sendMessage(msg);
+                        }
+
+                        @Override
+                        public IBinder asBinder() {
+                            return null;
+                        }
+                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
